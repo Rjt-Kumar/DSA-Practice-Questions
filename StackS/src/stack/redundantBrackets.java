5c34bdfb3d57f8395654f93a8efa576e1c48e35c
Rajat <cdrrjt@gmail.com>
2022-10-11 19:09:24 +0530

    Edit

--- StackS/src/stack/redundantBrackets.java
+++ StackS/src/stack/redundantBrackets.java
@@ -1,8 +1,37 @@
 package stack;
+import java.util.*;
 
 public class redundantBrackets {
 
     public static void main(String[] args) {
-        
+
+        Stack<Character> stack = new Stack<>();
+
+        String s = "((a+b))";
+
+        for(int i = 0; i < s.length();i++ ){
+
+            char ch = s.charAt(i);
+
+            // if it is an open bracket or an operator then push it into the stack
+            if( ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/' ){
+                stack.push(ch);
+            }else{
+
+                // ignore alphabets
+                if( ch == ')' ){
+                    // then check if under it contains any operator
+                    if( stack.peek() == '+'||  stack.peek() == '-'||stack.peek() == '*'||stack.peek() == '/'){
+                        while( stack.pop() != '('){ continue; }
+                    }else{
+                        System.out.println("Redundant brackets are present");
+                        return;
+                    }
+
+                }
+            }
+        }
+
+        System.out.println("Redundant brackets not present");
     }
 }
